eefea7d643843a4572d22b08d8962c233b7aca61
dotfury <cm2jr4@gmail.com>
2025-07-23 13:02:04 +0900

    add visibility checks - 2025/07/23

--- background-initial/public/scripts/background.js
+++ background-initial/public/scripts/background.js
@@ -1,5 +1,17 @@
 // Page Visibility API
-
+let backgroundInitialTimestamp;
+window.addEventListener('visibilitychange', (e) => {
+  if (document.visibilityState === "hidden") {
+    // in background
+    const now = new Date().toLocaleTimeString();
+    log(`Going to background at ${now}`);
+    backgroundInitialTimestamp = performance.now();
+  } else {
+    // back from background
+    const timeElapsed = Math.round(performance.now() - backgroundInitialTimestamp);
+    log(`Back from the background after ${timeElapsed/1000}s`);
+  }
+});
 
 // Beacon
 document.getElementById("btnBeacon").addEventListener("click", event => {
